c20efb13d08af42f9334b03b0d8c294ea2ae4d27
peilynn <38458767+peilynn@users.noreply.github.com>
2018-04-17 17:48:07 +0800

    Revert "123"
    This reverts commit 01a1839c3b37499503050b6a00f551661cdbe15b.

---
+++ WpfAppAirLineReservation/WpfAppAirLineReservation/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppAirLineReservation
+{
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            this.ShutdownMode = ShutdownMode.OnLastWindowClose;
+
+            //AirLineReservation W = new AirLineReservation();
+            //W.Show();
+
+            MainWindow W = new MainWindow();
+            W.ShowDialog();
+            if (W.DialogResult == true)
+            {
+                W.Close();
+                選擇航班 form = new 選擇航班();
+                form.ShowDialog();
+            }
+            
+        }
+    }
+}
